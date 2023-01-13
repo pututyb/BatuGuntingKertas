00942b8770982dc8fe9a78c6458098ff53e94ac2
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-01-13 13:45:26 +0700

    Initial Commit

---
+++ BatuGuntingKertas/BatuGuntingKertasApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatuGuntingKertasApp.swift
+//  BatuGuntingKertas
+//
+//  Created by Putut Yusri Bahtiar on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BatuGuntingKertasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
